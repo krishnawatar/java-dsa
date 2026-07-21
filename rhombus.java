00b955012c50ba45a7633bd9fd728534782e6159
krishnawatar <krishnawatartripathi@gmail.com>
2026-07-22 00:33:09 +0530

    added diamond

---
+++ rhombus.java
@@ -0,0 +1,35 @@
+public class rhombus {
+    public static void rhombus(int n){
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            
+            
+            System.out.println();
+        }
+    }
+    public static void hollowrom(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                if(i==1||i==n||j==1||j==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        hollowrom(5);
+    }
+}
